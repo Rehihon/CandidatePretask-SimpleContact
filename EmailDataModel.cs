1e11d69eb17846191d7c23d78ed557e0813c15c6
Rehihon <137357982+Rehihon@users.noreply.github.com>
2023-06-22 01:26:52 +0100

    Richard Ehihon Changes:
    Changes in Homecontroller, Index view and  and a new EmailModel

---
+++ EmailDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleContact.Models
+{
+    public class EmailDataModel
+    {      
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Message { get; set; }
+
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleContact.Models;
+using SimpleContact.Services.Implementation;
+using System.Diagnostics;
+
+namespace SimpleContact.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  ActionResult Index(EmailDataModel data)
+        {
+            try
+            {
+                EmailService emailService = new EmailService();
+                emailService.SendContactEmail(data.Name, data.Email, data.Message);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {   
+                ViewBag.Error = ex.Message;
+                return View(data);
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
